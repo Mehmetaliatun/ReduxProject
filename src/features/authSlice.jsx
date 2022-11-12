d8f015dd3122a9d83bfd4287e9ba153570cd0339
Atun <mali1996ua@gmail.com>
2022-11-12 12:39:45 +0300

    store

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState, // if key and object same, we can write one of them with es6
+  reducers: {
+    //aciton type'lari alir ve degistirir.
+    //reducer obje, key ve value aliyor(syntax bu sekilde).
+    setUser: (state, { payload }) => {
+      state.user = payload; // normalde action.payload olmasi gerekirken dest. yaptik.
+    },
+    clearUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+//obje icinde const ile tanimlayamadigimiz icin export yaparken const oluyor(func)
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
+
+//* slice metodu ile bunlari yazmamiza gerek kalmadi.
+// export const SET_USER = "SET_USER"
+
+// export const setUser=(payload)=>{
+//     return {type:SET_USER, payload }
+// }
